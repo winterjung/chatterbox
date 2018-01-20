5f2cfe03c358e39087b37af67c69e9f4484b7df7
jungwinter <wintermy201@gmail.com>
2018-01-21 00:04:35 +0900

    Setup: Add long description

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, find_packages
 
 
+with open('README.rst') as fp:
+    long_description = '\n' + fp.read()
+
 setup(
-    name='chatterbox',
+    name='chatterbox.py',
     version='0.0.1',
     description='Python library for Kakaotalk chatbot',
-    long_description='long_description',
+    long_description=long_description,
     author='JungWinter',
     author_email='wintermy201@gmail.com',
     url='https://github.com/JungWinter/chatterbox',
     packages=find_packages(exclude=('tests',)),
+    py_modules=['chatterbox'],
     install_requires=[],
     tests_require=["pytest", "pylint", "tox", "pytest-cov"],
     include_package_data=True,
