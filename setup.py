790cdeb1cfd465e42e03e4390313b63414968c3b
jungwinter <wintermy201@gmail.com>
2018-01-22 23:45:27 +0900

    Setup: replace the long description and add exclude folders

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as fp:
-    long_description = '\n' + fp.read()
-
 setup(
     name='chatterbox.py',
     version='0.0.1',
     description='Python library for Kakaotalk chatbot',
-    long_description=long_description,
+    long_description='Help to make chatbot using a state machine.',
     author='JungWinter',
     author_email='wintermy201@gmail.com',
     url='https://github.com/JungWinter/chatterbox',
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=('tests', 'examples', 'concept')),
     py_modules=['chatterbox'],
     install_requires=[],
     tests_require=["pytest", "pylint", "tox", "pytest-cov"],
