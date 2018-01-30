c9fdf4ff44d37810ca0766021e9a2ecba0f738a1
jungwinter <wintermy201@gmail.com>
2018-01-30 20:06:13 +0900

    Chatter: add basic methods about rule

--- chatterbox/chatter.py
+++ chatterbox/chatter.py
@@ -1,2 +1,13 @@
 class Chatter:
-    pass
+    def __init__(self):
+        self.rules = {}
+
+    def add_rule(self, action, src, dest, func):
+        rule_name = '{}_{}_{}'.format(action, src, dest)
+        self.rules[rule_name] = func
+
+    def rule(self, action, src, dest):
+        def decorator(func):
+            self.add_rule(action, src, dest, func)
+            return func
+        return decorator
