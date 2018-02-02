103170a624e484d585364bff361c448331306e74
jungwinter <wintermy201@gmail.com>
2018-02-02 18:56:49 +0900

    Tests: Add Test case about combination with Keyboard

--- tests/test_response.py
+++ tests/test_response.py
@@ -117,7 +117,61 @@ class TestResponse:
         assert text_dict + photo + button == self.response['full']
         assert text + photo_dict + button == self.response['full']
         assert text + photo + button_dict == self.response['full']
-        assert text + photo_dict + button_dict == self.response['full']
-        assert text_dict + photo + button_dict == self.response['full']
-        with pytest.raises(TypeError):
-            assert text_dict + photo_dict + button == self.response['full']
+
+    def test_message_keyboard_combination(self):
+        text = self.response_ins['text']
+        photo = self.response_ins['photo']
+        button = self.response_ins['message_button']
+        input_keyboard = self.response_ins['input_keyboard']
+        button_keyboard = self.response_ins['button_keyboard']
+
+        keyboards = (input_keyboard, button_keyboard)
+        messages = (text, photo, button)
+
+        repeats = range(1, len(messages)+1)
+        combination = (combinations(messages, r) for r in repeats)
+        flatten = chain(*combination)
+        adder = partial(reduce, add)
+        messages = map(adder, flatten)
+        matrix = product(messages, keyboards)
+
+        for message, keyboard in matrix:
+            assert message + keyboard == {**message, **keyboard}
+
+        combination = permutations((text, photo, button_keyboard))
+        for combo in combination:
+            assert adder(combo) == Response(**(text+photo), **button_keyboard)
+
+    def test_supported_operand(self):
+        text = self.response_ins['text']
+        photo = self.response_ins['photo']
+        button = self.response_ins['message_button']
+        keyboard = self.response_ins['button_keyboard']
+
+        assert isinstance(text + photo, Message)
+        assert isinstance(text + button, Message)
+        assert isinstance(photo + button, Message)
+        assert isinstance(text + photo + button, Message)
+
+        assert type(text + keyboard) is Response
+        assert type(photo + keyboard) is Response
+        assert type(button + keyboard) is Response
+        assert type(Message() + keyboard) is Response
+
+        assert type(keyboard + text) is Response
+        assert type(keyboard + photo) is Response
+        assert type(keyboard + button) is Response
+        assert type(keyboard + Message()) is Response
+
+    def test_unsupported_operand(self):
+        with pytest.raises(TypeError) as excinfo:
+            Keyboard([]) + Keyboard([])
+        assert 'Keyboard + Keyboard' in str(excinfo.value)
+
+        with pytest.raises(TypeError) as excinfo:
+            Response() + Keyboard([])
+        assert 'Response + Keyboard' in str(excinfo.value)
+
+        with pytest.raises(TypeError) as excinfo:
+            Response() + Response()
+        assert 'Response + Response' in str(excinfo.value)
