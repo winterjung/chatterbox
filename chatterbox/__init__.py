c72dd9a3e104b457b273727850f6f62d1dd3a35d
jungwinter <wintermy201@gmail.com>
2018-02-02 18:58:54 +0900

    Response: Implement Response to combine with Keyboard

--- chatterbox/__init__.py
+++ chatterbox/__init__.py
@@ -3,10 +3,12 @@ __email__ = 'wintermy201@gmail.com'
 
 
 from chatterbox.chatter import Chatter
-from chatterbox.response import Keyboard, Message, Text, Photo, MessageButton
+from chatterbox.response import (Response, Keyboard, Message,
+                                 Text, Photo, MessageButton)
 
 __all__ = [
     'Chatter',
+    'Response',
     'Keyboard',
     'Message',
     'Text',
--- chatterbox/response.py
+++ chatterbox/response.py
@@ -1,24 +1,52 @@
-class Message(dict):
-    def __init__(self, text=None, photo=None, message_button=None):
-        message_dict = dict()
+class Response(dict):
+    def __init__(self, message=None, keyboard=None):
+        self._message = message
+        self._keyboard = keyboard
+        response = dict()
 
-        if text:
-            message_dict['text'] = text
-        if photo:
-            message_dict['photo'] = photo
-        if message_button:
-            message_dict['message_button'] = message_button
+        if self._message:
+            response['message'] = self._message
+        if self._keyboard:
+            response['keyboard'] = self._keyboard
 
-        super().__init__(self, message=message_dict)
+        super().__init__(**response)
 
     def __add__(self, other):
-        return Message(**self['message'], **other['message'])
+        if isinstance(other, Keyboard):
+            raise TypeError('unsupported operand: Response + Keyboard')
+        if type(other) is Response:
+            raise TypeError('unsupported operand: Response + Response')
+        return Response(message={**self['message'], **other['message']},
+                        keyboard=self['keyboard'])
 
     def __radd__(self, other):
         return self.__add__(other)
 
 
-class Keyboard(dict):
+class Message(Response):
+    def __init__(self, text=None, photo=None, message_button=None):
+        self._text = text
+        self._photo = photo
+        self._message_button = message_button
+
+        message_dict = dict()
+
+        if self._text:
+            message_dict['text'] = self._text
+        if self._photo:
+            message_dict['photo'] = self._photo
+        if self._message_button:
+            message_dict['message_button'] = self._message_button
+
+        super().__init__(message=message_dict)
+
+    def __add__(self, other):
+        if isinstance(other, Keyboard):
+            return Response(**self, **other)
+        return Message(**self['message'], **other['message'])
+
+
+class Keyboard(Response):
     def __init__(self, buttons=None, type='buttons'):
         self._buttons = buttons
         self._type = type
@@ -28,7 +56,7 @@ class Keyboard(dict):
             'text': self._text_type_keyboard(),
             'buttons': self._buttons_type_keyboard(),
         }
-        super().__init__(self, keyboard=button_mapping[self._type])
+        super().__init__(keyboard=button_mapping[self._type])
 
     def _check_validity(self):
         if not isinstance(self._buttons, list) and self._type == 'buttons':
@@ -41,6 +69,11 @@ class Keyboard(dict):
 
     def _buttons_type_keyboard(self):
         return dict(type='buttons', buttons=self._buttons)
+
+    def __add__(self, other):
+        if isinstance(other, Keyboard):
+            raise TypeError('unsupported operand: Keyboard + Keyboard')
+        return Response(**other, **self)
 
 
 class Text(Message):
