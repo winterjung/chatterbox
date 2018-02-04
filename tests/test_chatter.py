e25a47b3f18e23bc8146f3698fb7fb9a40aaca8a
jungwinter <wintermy201@gmail.com>
2018-02-04 12:44:33 +0900

    Tests: Replace to fixture

--- tests/test_chatter.py
+++ tests/test_chatter.py
@@ -2,11 +2,6 @@ from chatterbox import Chatter
 
 
 class TestChatter:
-
-    @classmethod
-    def setup_class(cls):
-        pass
-
     def test_create_chatter(self):
         chatter = Chatter()
         assert chatter is not None
@@ -17,9 +12,7 @@ class TestChatter:
         for func_name in handler:
             assert callable(handler[func_name])
 
-    def test_add_rule_with_kwargs(self, handler):
-        chatter = Chatter()
-
+    def test_add_rule_with_kwargs(self, chatter, handler):
         chatter.add_rule(action='자기소개',
                          src='홈',
                          dest='소개',
@@ -28,24 +21,16 @@ class TestChatter:
         assert chatter.rules is not None
         assert chatter.rules['자기소개_홈_소개'] is not None
 
-    def test_add_rule_without_kwargs(self, handler):
-        chatter = Chatter()
-
+    def test_add_rule_without_kwargs(self, chatter, handler):
         chatter.add_rule('자기소개', '홈', '소개', handler['intro'])
 
         assert chatter.rules is not None
         assert chatter.rules['자기소개_홈_소개'] is not None
 
-    def test_add_rule_decorator(self):
-        chatter = Chatter()
-
+    def test_add_rule_decorator(self, chatter):
         @chatter.rule(action='자기소개', src='홈', dest='소개')
         def intro(data):
             pass
 
         assert chatter.rules is not None
         assert chatter.rules['자기소개_홈_소개'] is not None
-
-    @classmethod
-    def teardown_class(cls):
-        pass
