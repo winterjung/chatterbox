d48fe21fb7650b74828cc10468d1661210b0fcae
jungwinter <wintermy201@gmail.com>
2018-02-24 16:18:21 +0900

    Chatterbox: Release v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+[Chatterbox] 프로젝트의 모든 변경 사항이 이 파일에 기록됩니다.
+
+- `New`: 새로운 구현 사항
+- `Fix`: 잘못된 부분 변경
+- `Change`: 작동 방식 변경
+
+## 0.1.0 - 2018-02-24
+### New
+:tada:
+
+[Chatterbox]: https://github.com/jungwinter/chatterbox
--- setup.py
+++ setup.py
@@ -1,11 +1,51 @@
-from setuptools import setup, find_packages
+import os
+import sys
+from shutil import rmtree
+from setuptools import setup, find_packages, Command
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(
+            sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
 
 
 setup(
     name='chatterbox.py',
-    version='0.0.1',
+    version='0.1.0',
     description='Python library for Kakaotalk chatbot',
-    long_description='Help to make chatbot using a state machine.',
+    long_description='Help to make kakaotalk chatbot using a state machine.',
     author='JungWinter',
     author_email='wintermy201@gmail.com',
     url='https://github.com/JungWinter/chatterbox',
@@ -14,5 +54,22 @@ setup(
     install_requires=[],
     tests_require=["pytest", "pylint", "tox", "pytest-cov"],
     include_package_data=True,
-    license='MIT'
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Korean',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
