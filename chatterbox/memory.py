80cfd52a28d53b8372b84e69fa7bd88900d812e3
jungwinter <wintermy201@gmail.com>
2018-02-24 15:26:06 +0900

    Memory: Support the new memory using sqlite3

--- chatterbox/memory.py
+++ chatterbox/memory.py
@@ -99,6 +99,110 @@ class DictionaryMemory(BaseMemory):
             self.user_list.pop(key, None)
 
 
+class SqliteMemory(BaseMemory):
+    TIMEOUT = 10 * 60
+
+    def __init__(self, frequency):
+        self.path = SqliteMemory.db_path()
+        self.init_db()
+        self.frequency = frequency
+
+        atexit.register(SqliteMemory.remove_db, self.path)
+
+    @staticmethod
+    def db_path(name='chatterbox.db'):
+        temp = tempfile.gettempdir()
+        path = pathlib.Path(temp, name)
+        return path
+
+    @staticmethod
+    def remove_db(db=None):
+        if db is None:
+            db = SqliteMemory.db_path()
+        try:
+            db.unlink()
+        except FileNotFoundError:
+            pass
+
+    @contextlib.contextmanager
+    def cursor(self):
+        con = sqlite3.connect(str(self.path))
+        with contextlib.closing(con):
+            with con:
+                yield con.cursor()
+
+    def init_db(self):
+        SqliteMemory.remove_db(self.path)
+
+        with self.cursor() as cur:
+            cur.execute('''CREATE TABLE if not exists UserList (
+                user_key text PRIMARY KEY,
+                user text,
+                last_time integer
+            )''')
+
+    def user(self, user_key):
+        value = (user_key, )
+        with self.cursor() as cur:
+            cur.execute('''SELECT user
+                FROM UserList
+                WHERE user_key = ?
+            ''', value)
+            user = cur.fetchone()
+
+        if user is not None:
+            user = User.from_json(user[0])
+        return user
+
+    @count
+    def create(self, user_key, current_state):
+        value = (
+            user_key,
+            User(user_key, current_state).to_json(),
+            now(),
+        )
+        with self.cursor() as cur:
+            cur.execute('INSERT INTO UserList VALUES (?, ?, ?)', value)
+
+        return self.user(user_key)
+
+    @count
+    def save(self, user):
+        value = (
+            user.to_json(),
+            now(),
+            user.user_key,
+        )
+        with self.cursor() as cur:
+            cur.execute('''UPDATE UserList
+                SET user = ?,
+                    last_time = ?
+                WHERE user_key = ?
+            ''', value)
+
+    @count
+    def delete(self, user):
+        value = (
+            user.user_key,
+        )
+        with self.cursor() as cur:
+            cur.execute('''DELETE FROM UserList
+                WHERE user_key = ?
+            ''', value)
+
+    def collect(self):
+        utcnow = now()
+        value = (
+            utcnow,
+            self.TIMEOUT,
+        )
+
+        with self.cursor() as cur:
+            cur.execute('''DELETE FROM UserList
+                WHERE ? - last_time > ?
+            ''', value)
+
+
 class User(SimpleNamespace):
     def __init__(self, user_key, current=None, previous=None):
         self.user_key = user_key
@@ -117,3 +221,9 @@ class User(SimpleNamespace):
     def from_json(cls, obj):
         user = User(**json.loads(obj))
         return user
+
+
+available = {  # pylint: disable=invalid-name
+    'dict': DictionaryMemory,
+    'sqlite': SqliteMemory,
+}
