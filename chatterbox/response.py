70a86e85e3ba858f3d8ff3ef161712e389e1fdf0
jungwinter <wintermy201@gmail.com>
2018-02-25 19:33:12 +0900

    Response: Now dot access is available

--- chatterbox/response.py
+++ chatterbox/response.py
@@ -1,122 +1,178 @@
-class Response(dict):
-    def __init__(self, message=None, keyboard=None):
-        self._message = message
-        self._keyboard = keyboard
-        response = dict()
+import abc
+from collections import ChainMap
 
-        if self._message:
-            response['message'] = self._message
-        if self._keyboard:
-            response['keyboard'] = self._keyboard
 
-        super().__init__(**response)
-
+class Addable(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
     def __add__(self, other):
-        if isinstance(other, Keyboard):
-            raise TypeError('unsupported operand: Response + Keyboard')
-        if type(other) is Response:
-            raise TypeError('unsupported operand: Response + Response')
-        return Response(message={**self['message'], **other['message']},
-                        keyboard=self['keyboard'])
+        pass
 
     def __radd__(self, other):
         return self.__add__(other)
 
 
-class Message(Response):
-    def __init__(self, text=None, photo=None, message_button=None):
-        self._mapping = {
-            'text': text,
-            'photo': photo,
-            'message_button': message_button,
-        }
-
-        message_dict = dict()
-
-        for category, msg in self._mapping.items():
-            if msg is None:
-                continue
-            msg = self._unpack(msg, category)
-            message_dict[category] = msg
-
-        super().__init__(message=message_dict)
-
-    def _unpack(self, message, category):
-        if isinstance(message, Message):
-            message = message['message'][category]
-        return message
-
+class MessageAddMixin(Addable):
     def __add__(self, other):
         if isinstance(other, Keyboard):
-            return Response(**self, **other)
-        return Message(**self['message'], **other['message'])
+            return Response(self.message, other.keyboard)
+        return Message(**ChainMap(self.message, other.message))
 
 
-class Keyboard(Response):
-    def __init__(self, buttons=None, type='buttons'):
-        self._buttons = buttons
-        self._type = type
-        self._check_validity()
-
-        button_mapping = {
-            'text': self._text_type_keyboard(),
-            'buttons': self._buttons_type_keyboard(),
-        }
-        super().__init__(keyboard=button_mapping[self._type])
-
-    def _check_validity(self):
-        if not isinstance(self._buttons, list) and self._type == 'buttons':
-            raise TypeError('buttons must be list in buttons type')
-        if self._buttons is not None and self._type == 'text':
-            raise TypeError('buttons must be None in text type')
-
-    def _text_type_keyboard(self):
-        return dict(type='text')
-
-    def _buttons_type_keyboard(self):
-        return dict(type='buttons', buttons=self._buttons)
-
+class KeyboardAddMixin(Addable):
     def __add__(self, other):
         if isinstance(other, Keyboard):
             raise TypeError('unsupported operand: Keyboard + Keyboard')
-        return Response(**other, **self)
+        return Response(other.message, self.keyboard)
 
 
-class Text(Message):
+class ResponseAddMixin(Addable):
+    def __add__(self, other):
+        if isinstance(other, Keyboard):
+            raise TypeError('unsupported operand: Response + Keyboard')
+        if type(other) is Response:
+            raise TypeError('unsupported operand: Response + Response')
+        message = dict(ChainMap(self.message.message, other.message))
+        keyboard = self.keyboard.keyboard
+        return Response(message=message,
+                        keyboard=keyboard)
+
+
+class Response(dict, ResponseAddMixin):
+    def __init__(self, message=None, keyboard=None):
+        self.message = None
+        self.keyboard = None
+
+        if message:
+            self.message = Message(**message)
+        if keyboard:
+            self.keyboard = Keyboard(**keyboard)
+
+        self.response = self.frame()
+        super().__init__(**self.response)
+
+    def frame(self):
+        response = ChainMap()
+        if self.message:
+            response = response.new_child(self.message)
+        if self.keyboard:
+            response = response.new_child(self.keyboard)
+        return dict(response)
+
+
+class Message(dict, MessageAddMixin):
+    def __init__(self, text=None, photo=None, message_button=None):
+        self.text = None
+        self.photo = None
+        self.message_button = None
+
+        if isinstance(text, Text):
+            self.text = text
+        elif text:
+            self.text = Text(text)
+
+        if isinstance(photo, Photo):
+            self.photo = photo
+        elif photo:
+            self.photo = Photo(**photo)
+
+        if isinstance(message_button, MessageButton):
+            self.message_button = message_button
+        elif message_button:
+            self.message_button = MessageButton(**message_button)
+
+        self.message = self.frame()
+        super().__init__(message=self.message)
+
+    def frame(self):
+        messages = (self.text, self.photo, self.message_button)
+        not_none_messages = (msg.message for msg in messages if msg)
+        return dict(ChainMap(*not_none_messages))
+
+
+class Keyboard(dict, KeyboardAddMixin):
+    def __init__(self,
+                 buttons=None,
+                 type='buttons'):  # pylint: disable=redefined-builtin
+        self.buttons = buttons
+        self.type = type
+        self.validate()
+
+        self.keyboard = self.frame()
+        super().__init__(keyboard=self.keyboard)
+
+    def validate(self):
+        if not isinstance(self.buttons, list) and self.type == 'buttons':
+            raise TypeError('buttons must be list in buttons type')
+        if self.buttons is not None and self.type == 'text':
+            raise TypeError('buttons must be None in text type')
+
+    def frame(self):
+        mapping = {
+            'text': self.generate_text_keyboard(),
+            'buttons': self.generate_buttons_keyboard(self.buttons),
+        }
+        return mapping.get(self.type)
+
+    @classmethod
+    def generate_text_keyboard(cls):
+        return dict(type='text')
+
+    @classmethod
+    def generate_buttons_keyboard(cls, buttons):
+        return dict(type='buttons', buttons=buttons)
+
+
+class Text(dict, MessageAddMixin):
     def __init__(self, text):
-        self._text = text
+        self.text = text
 
-        text_dict = {
-            'text': self._text
+        self.message = self.frame()
+        super().__init__(message=self.message)
+
+    def frame(self):
+        return {
+            'text': self.text
         }
-        super().__init__(**text_dict)
 
 
-class Photo(Message):
+class Photo(dict, MessageAddMixin):
     def __init__(self, url, width, height):
-        self._url = url
-        self._width = width
-        self._height = height
+        self.url = url
+        self.width = width
+        self.height = height
+        self.validate()
 
-        photo_dict = {
+        self.message = self.frame()
+        super().__init__(message=self.message)
+
+    def validate(self):
+        if not isinstance(self.width, int):
+            raise TypeError('width must be int')
+        if not isinstance(self.height, int):
+            raise TypeError('height must be int')
+
+    def frame(self):
+        return {
             'photo': {
-                'url': self._url,
-                'width': self._width,
-                'height': self._height,
+                'url': self.url,
+                'width': self.width,
+                'height': self.height,
             }
         }
-        super().__init__(**photo_dict)
 
 
-class MessageButton(Message):
+class MessageButton(dict, MessageAddMixin):
     def __init__(self, label, url):
-        self._label = label
-        self._url = url
+        self.label = label
+        self.url = url
 
-        message_button_dict = {
+        self.message = self.frame()
+        super().__init__(message=self.message)
+
+    def frame(self):
+        return {
             'message_button': {
-                'label': self._label,
-                'url': self._url,
+                'label': self.label,
+                'url': self.url,
             }
         }
-        super().__init__(**message_button_dict)
